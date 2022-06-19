468c9f827071f9a83c0ece1d0150037128685f19
neeboo <ghostcorn@gmail.com>
2022-06-19 19:45:31 +0800

    :sparkles: use local Identity

--- packages/adapter/src/identity.ts
+++ packages/adapter/src/identity.ts
@@ -1,16 +1,14 @@
 import { ICPSnapApi, SignRawMessageResponse, SignMessageResponse } from '@astrox/icsnap-types';
 import { PublicKey, Signature, SignIdentity } from '@dfinity/agent';
 import { fromHexString, toHexString } from './util';
-import { Secp256k1PublicKey } from '@dfinity/identity';
+import { Secp256k1KeyIdentity, Secp256k1PublicKey } from '@dfinity/identity';
 import { Principal } from '@dfinity/principal';
 
 export class SnapIdentity extends SignIdentity {
   constructor(private _api: ICPSnapApi, private _rawPublickeyString: string, private _principalString: string) {
     super();
   }
-
   getPublicKey(): PublicKey {
-    console.log('from api: ' + this._rawPublickeyString);
     return Secp256k1PublicKey.fromRaw(fromHexString(this._rawPublickeyString));
   }
 
@@ -20,14 +18,20 @@ export class SnapIdentity extends SignIdentity {
 
   async sign(blob: ArrayBuffer): Promise<Signature> {
     try {
-      const signedResponse = await this._api.sign(toHexString(blob));
-      if (signedResponse.error) {
-        throw signedResponse.error;
-      }
-      if (signedResponse.confirmed === false) {
-        throw new Error(`signing message error: signing process terminated by user`);
-      }
-      return fromHexString(signedResponse.signature) as Signature;
+      // console.log(toHexString(blob));
+
+      // const signedResponse = await this._api.sign(toHexString(blob));
+      // if (signedResponse.error) {
+      //   throw signedResponse.error;
+      // }
+      // if (signedResponse.confirmed === false) {
+      //   throw new Error(`signing message error: signing process terminated by user`);
+      // }
+      // return fromHexString(signedResponse.signature) as Signature;
+
+      const _innerIdentity = Secp256k1KeyIdentity.fromJSON(await this._api.getIdentity());
+      const sig = await _innerIdentity.sign(blob);
+      return sig;
     } catch (error) {
       throw new Error(`signing message error: ${error.message}`);
     }
